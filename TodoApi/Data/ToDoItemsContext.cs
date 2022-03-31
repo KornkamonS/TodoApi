ce2dfcb076986437ffdef8dc37d582f747eb9d29
TUNASANG\Kornk <Kornkamon.s@outlook.com>
2022-03-31 23:26:52 +0700

    Add project files.

---
+++ TodoApi/Data/ToDoItemsContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace TodoAPI.Data
+{
+    public class ToDoItemsContext : DbContext
+    {
+        public ToDoItemsContext (DbContextOptions<ToDoItemsContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TodoItems> TodoItems { get; set; }
+    }
+}
---
+++ TodoApiTest/ContextInit.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TodoAPI.Data; 
+
+namespace TestAPI
+{
+    public static class DbContextMocker
+    {
+        public static void Seed(this ToDoItemsContext dbContext)
+        { 
+            dbContext.TodoItems.Add(new TodoItems
+            {
+                Id = 1,
+                Name = "A"
+            });
+            dbContext.SaveChanges();
+        }
+
+        public static ToDoItemsContext CreateTestingDatabase(string dbName)
+        { 
+            var options = new DbContextOptionsBuilder<ToDoItemsContext>()
+                .UseInMemoryDatabase(databaseName: dbName)
+                .Options;
+             
+            var dbContext = new ToDoItemsContext(options); 
+            dbContext.Seed();
+
+            return dbContext;
+        }
+    }
+}
